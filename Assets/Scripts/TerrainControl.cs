6d794f9e8a582d3b443cdeac8af1fad98cc4c44a
Dovakiin <augustinevana@gmail.com>
2016-08-17 16:29:34 +0800

    Commit SafeHouse Project

---
+++ Assets/Scripts/TerrainControl.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TerrainControl : MonoBehaviour
+{
+    public TerrainData terrain;
+
+    private void drawTerrainTexture()
+    {
+        int height = terrain.alphamapHeight;
+        int width = terrain.alphamapWidth;
+
+        float[, ,] alphamap = new float[width , height , 2];
+
+        for ( int y = 0 ; y < height ; y++ )
+        {
+            for ( int x = 0 ; x < width ; x++ )
+            {
+                alphamap[y , x , 0] = (float)x / width;
+                alphamap[y , x , 1] = 1 - (float)x / width;
+            }
+        }
+
+        terrain.SetAlphamaps( 0 , 0 , alphamap );
+    }
+
+    void OnGUI()
+    {
+        if ( GUI.Button( new Rect( 0 , 0 , 150 , 50 ) , "DrawTexture" ) )
+        {
+            drawTerrainTexture();
+        }
+    }
+}
